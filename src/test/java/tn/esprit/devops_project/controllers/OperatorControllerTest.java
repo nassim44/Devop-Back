0298a185f467c2a660da86b11a4f1f394b4be951
nassim44 <nassim.benali@esprit.tn>
2024-04-11 16:05:17 +0100

    push back update 10

--- src/test/java/tn/esprit/devops_project/controllers/OperatorControllerTest.java
+++ src/test/java/tn/esprit/devops_project/controllers/OperatorControllerTest.java
@@ -7,35 +7,35 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import tn.esprit.devops_project.entities.Operator;
 import tn.esprit.devops_project.repositories.OperatorRepository;
 import tn.esprit.devops_project.services.OperatorServiceImpl;
-import static org.mockito.Mockito.mock;
+
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
 class OperatorControllerTest {
     @Mock
     private OperatorRepository operatorRepositoryMock;
-
+    @InjectMocks
+    private OperatorController operatorController;
     @InjectMocks
     private OperatorServiceImpl operatorService;
 
     @Test
     public void testRetrieveAllOperators() {
-        // Arrange
         List<Operator> expectedOperators = Arrays.asList(
                 new Operator(1L, "Operator A","A","nassim123", new HashSet<>()),
                 new Operator(2L, "Operator B","B","nassim123", new HashSet<>())
-                // Add more operators as needed
         );
 
         when(operatorRepositoryMock.findAll()).thenReturn(expectedOperators);
@@ -44,5 +44,31 @@ class OperatorControllerTest {
         assertEquals(expectedOperators.size(), actualOperators.size());
         assertEquals(expectedOperators, actualOperators);
 
+    }
+
+    @Test
+    void addOperator() {
+        Operator newOperator = new Operator(1L,"operator 1" ,"1" ,"nassim123",new HashSet<>());
+        when(operatorService.addOperator(newOperator)).thenReturn(newOperator);
+
+        Operator addedOperator = operatorController.addOperator(newOperator);
+
+        assertEquals(newOperator, addedOperator);
+    }
+
+    @Test
+    void removeOperator() {
+        Long operatorId = 1L;
+        operatorController.removeOperator(operatorId);
+        verify(operatorService, times(1)).deleteOperator(operatorId);
+    }
+
+
+    @Test
+    void modifyOperateur() {
+        Operator modifiedOperator = new Operator(1L,"operator 1" ,"1" ,"nassim123",new HashSet<>());
+        when(operatorService.updateOperator(modifiedOperator)).thenReturn(modifiedOperator);
+        Operator updatedOperator  = operatorController.modifyOperateur(modifiedOperator);
+        assertEquals(modifiedOperator, updatedOperator);
     }
 }
